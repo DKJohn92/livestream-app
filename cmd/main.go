ec2092bc4c71eea80b320af0983d403e86eff289
kerschl <kerschl@publicplan.de>
2024-06-14 09:36:23 +0200

    feat: 🎸 add websocket connection and templates js/html/css

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"log"
+
+	"livestream-app/internal/server"
+)
+
+func main() {
+	if err := server.Run(); err != nil {
+		log.Fatalln(err.Error())
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,11 +5,14 @@ import (
 	"os"
 	"time"
 
-	"github.com/gofiber/contrib/websocket"
-	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/cors"
-	"github.com/gofiber/fiber/v3/middleware/logger"
-	"github.com/gofiber/template/html/v2"
+	"livestream-app/internal/handlers"
+	w "livestream-app/pkg/webrtc"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
 var (
@@ -25,13 +28,13 @@ func Run() error {
 		*addr = ":8080"
 	}
 
-	engine := html.New("/.views", ".html")
+	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
 	app.Use(logger.New())
 	app.Use(cors.New())
 
 	app.Get("/", handlers.Welcome)
-	app.Get("room/create", handlers.RoomCreate)
+	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
 	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
 		HandshakeTimeout: 10 * time.Second,
@@ -39,8 +42,27 @@ func Run() error {
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
-	app.Get("/stream/:ssuid", handlers.Stream)
-	app.Get("/stream/:ssuid/websocket")
-	app.Get("/stream/:ssuid/chat/websocket")
-	app.Get("/stream/:ssuid/viewer/websocket")
+	app.Get("/stream/:suuid", handlers.Stream)
+	app.Get("/stream/:suuid/websocket", websocket.New(handlers.StreamWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
+	app.Get("/stream/:suuid/chat/websocket", websocket.New(handlers.StreamChatWebsocket))
+	app.Get("/stream/:suuid/viewer/websocket", websocket.New(handlers.StreamViewerWebsocket))
+	app.Static("/", "./assets")
+
+	w.Rooms = make(map[string]*w.Room)
+	w.Streams = make(map[string]*w.Room)
+	go dispatchKeyFrames()
+	if *cert != "" {
+		return app.ListenTLS(*addr, *cert, *key)
+	}
+	return app.Listen(*addr)
+}
+
+func dispatchKeyFrames() {
+	for range time.NewTicker(time.Second * 3).C {
+		for _, room := range w.Rooms {
+			room.Peers.DispatchKeyFrame()
+		}
+	}
 }
